ef8365adb3fb61586d5cca80603a41a6f61f9f46
Tushar <gaikwadtushar@gmail.com>
2026-04-19 18:28:12 +0530

    nothing change

--- src/main/java/com/FoodServe/Dilevery/config/CorsConfig.java
+++ src/main/java/com/FoodServe/Dilevery/config/CorsConfig.java
@@ -21,20 +21,7 @@ public class CorsConfig {
         };
     }
 	
-//	React connection code 
-//	@Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**")
-//                        .allowedOrigins("http://localhost:3000")
-//                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                        .allowedHeaders("*")
-//                        .allowCredentials(true);
-//            }
-//        };
-//    }
+
     
     
     
